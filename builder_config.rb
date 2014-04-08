e865e6c51cf1a87f75ed10d97e88457a04ccf44f
Damian Hamill <damianham@gmail.com>
2014-04-08 16:17:23 +0700

    add config file

---
+++ builder_config.rb
@@ -0,0 +1,15 @@
+
+
+# define the builders with their output destinations
+@builders = {
+  RailsBuilder => './myapp',
+  AngularRailsBuilder => './myapp/app/assets/javascripts'
+}
+
+# use RailsVanillaBuilder instead of RailsBuilder 
+# for standard rails artifacts using the rails scaffold generator
+
+
+# define the tables to ignore e.g. IGNORE_TABLES = ['dodgy_table1','dummy_table2']
+IGNORE_TABLES = []
+
